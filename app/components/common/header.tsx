97e77fd8c8d482021c8b3bb74e7a11ab932bc0f9
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-08-18 00:03:47 +0800

    feat: added sign out and conditionally render headr for sign in or  sign out

--- app/components/common/header.tsx
+++ app/components/common/header.tsx
@@ -1,17 +1,56 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
 import { routes } from "../utils/routes/routes";
+import { auth } from "../helpers/config";
+import { signOut } from "firebase/auth";
+import Spinner from "./spinner";
 
 function Header() {
   const pathname = usePathname();
   const router = useRouter();
   const routesWithHeaderNav = [routes.home];
+  const [jwtToken, setJwtToken] = useState<string | null>(null);
+  const [loadingUser, setLoadingUser] = useState(true);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  useEffect(() => {
+    const getToken = async () => {
+      setLoadingUser(true);
+      try {
+        auth.onAuthStateChanged(async (user) => {
+          if (user) {
+            const idToken = await user.getIdToken(true);
+            setJwtToken(idToken);
+          } else {
+            setJwtToken(null);
+          }
+          setLoadingUser(false);
+        });
+      } catch (error) {
+        console.error(error);
+        setJwtToken(null);
+        setLoadingUser(false);
+      }
+    };
+
+    getToken();
+  }, []);
+
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth);
+      setJwtToken(null);
+      router.replace(routes.home);
+    } catch (error) {
+      console.error("Error signing out:", error);
+    }
+  };
 
   return (
-    <div className="h-[75px] flex justify-between px-4 sm:px-6 lg:px-20 items-center text-white">
+    <div className="h-[75px] flex justify-between px-4 sm:px-6 lg:px-20 items-center text-white relative">
       <div
         className="flex gap-2 items-center cursor-pointer"
         onClick={() => {
@@ -30,7 +69,6 @@ function Header() {
       </div>
       {routesWithHeaderNav.includes(pathname) && (
         <div className="flex items-center gap-2 sm:gap-3 lg:gap-5 text-sm sm:text-base lg:text-lg font-semibold">
-          {/* Add any navigation links or icons here */}
           <div
             className="cursor-pointer text-primary-dark"
             onClick={() => {
@@ -39,27 +77,62 @@ function Header() {
           >
             Our Services
           </div>
-          <div className="h-[40px] flex items-center px-4 py-2 border-2 border-primary-dark text-primary-dark rounded-full bg-white">
-            <div>
-              <span
-                className="cursor-pointer"
-                onClick={() => {
-                  router.replace(routes.register);
-                }}
+          {loadingUser ? (
+            <Spinner type="secondary" />
+          ) : jwtToken ? (
+            <div className="relative">
+              <div
+                className="flex gap-1 text-primary-dark underline underline-offset-4 font-semibold cursor-pointer"
+                onClick={() => setDropdownOpen(!dropdownOpen)}
               >
-                Sign Up{" "}
-              </span>
-              |{" "}
-              <span
-                className="cursor-pointer"
-                onClick={() => {
-                  router.replace(routes.login);
-                }}
-              >
-                Sign In
-              </span>
+                {auth.currentUser?.email}
+                <Image
+                  width={16}
+                  height={16}
+                  src="/svg/dropdown-icon.svg"
+                  alt="dropdown-icon"
+                />
+              </div>
+              {dropdownOpen && (
+                <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-300 rounded-lg shadow-lg z-50">
+                  <div
+                    className="flex gap-1 justify-center px-4 py-2 text-primary-dark cursor-pointer hover:bg-gray-100 rounded-t-lg"
+                    onClick={handleSignOut}
+                  >
+                    Sign Out
+                    <Image
+                      width={16}
+                      height={16}
+                      src="/svg/sign-out-icon.svg"
+                      alt="signout-icon"
+                    />
+                  </div>
+                </div>
+              )}
             </div>
-          </div>
+          ) : (
+            <div className="h-[40px] flex items-center px-4 py-2 border-2 border-primary-dark text-primary-dark rounded-full bg-white">
+              <div>
+                <span
+                  className="cursor-pointer"
+                  onClick={() => {
+                    router.replace(routes.register);
+                  }}
+                >
+                  Sign Up{" "}
+                </span>
+                |{" "}
+                <span
+                  className="cursor-pointer"
+                  onClick={() => {
+                    router.replace(routes.login);
+                  }}
+                >
+                  Sign In
+                </span>
+              </div>
+            </div>
+          )}
         </div>
       )}
     </div>
