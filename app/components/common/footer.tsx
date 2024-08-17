4de377afea07c557e400a0ef2556c1b301c3c307
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-08-17 23:28:51 +0800

    feat: make footer responsive

--- app/components/common/footer.tsx
+++ app/components/common/footer.tsx
@@ -1,8 +1,10 @@
 function Footer() {
   return (
-    <div className="text-center py-10 text-primary-dark">
-      Copyright © {new Date().getFullYear()} - by QUALISOFT Solutions. All
-      rights reserved.
+    <div className="px-4 py-6 lg:py-10 text-center text-primary-dark">
+      <p className="text-sm lg:text-base">
+        Copyright © {new Date().getFullYear()} - by QUALISOFT Solutions. All
+        rights reserved.
+      </p>
     </div>
   );
 }
