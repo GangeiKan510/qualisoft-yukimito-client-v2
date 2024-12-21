43c89aa512a0cd1c87ec20b3a0221836ddd9ef2e
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-12-21 20:51:43 +0800

    fix: indexing issues

--- app/components/modals/booking-summary.tsx
+++ app/components/modals/booking-summary.tsx
@@ -74,7 +74,7 @@ const BookingSummary: React.FC<BookingSummaryProps> = ({
   };
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
       <div className="bg-white p-6 rounded-md w-full max-w-md">
         <h2 className="text-2xl font-bold mb-4 text-primary-dark">
           Booking Summary
