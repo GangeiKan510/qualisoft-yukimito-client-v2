fb03ebcfa63eabdf0b86a9e8f116786932897fb6
Kert Solas <kertsolas@gmail.com>
2025-01-25 16:23:19 +0800

    chore: removed date administered and changed batch number to description

--- components/modals/create-vaccine.tsx
+++ components/modals/create-vaccine.tsx
@@ -50,48 +50,53 @@ const CreateVaccineModal: React.FC<CreateVaccineModalProps> = ({
           ✕
         </button>
         <div className="flex flex-col text-center mt-3">
-          <div className="text-lg font-semibold text-black">Add Vaccine</div>
+          <div className="text-lg font-bold text-black">Add Vaccine</div>
           <div className="mt-4 space-y-4">
+          <label className="block text-left font-semibold mb-1px w-30">
+            Vaccine Name:
+          </label>
             <input
               type="text"
               name="name"
               value={vaccineData.name}
               onChange={handleChange}
-              placeholder="Vaccine Name"
+              placeholder="Ex: Vanguard Plus 5"
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring"
               disabled={loading}
             />
+            <label className="block text-left font-semibold mb-1px w-30">
+              Manufacturer:
+            </label>
             <input
               type="text"
               name="manufacturer"
               value={vaccineData.manufacturer}
               onChange={handleChange}
-              placeholder="Manufacturer"
+              placeholder="Ex: Zoetis "
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring"
               disabled={loading}
             />
+            <label className="block text-left font-semibold mb-1px w-30">
+              Description/Purpose:
+            </label>
             <input
               type="text"
               name="batch_number"
               value={vaccineData.batch_number}
               onChange={handleChange}
-              placeholder="Batch Number"
+              placeholder="Ex: This vaccine is for Rabies"
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring"
               disabled={loading}
             />
+            <label className="block text-left font-semibold mb-1px w-30">
+              Expiry Date:
+            </label>
             <input
               type="date"
               name="expiry_date"
               value={vaccineData.expiry_date}
               onChange={handleChange}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring"
-              disabled={loading}
-            />
-            <input
-              type="date"
-              name="date_administered"
-              value={vaccineData.date_administered}
-              onChange={handleChange}
+              placeholder="Expiry Date"
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring"
               disabled={loading}
             />
